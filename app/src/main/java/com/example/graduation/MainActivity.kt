3f62b24ec2f8c54c0246df3e960e7ac01f030420
DareeEssam <dareen2492002@gmail.com>
2024-04-18 03:08:05 +0200

    .

--- app/src/main/java/com/example/graduation/MainActivity.kt
+++ app/src/main/java/com/example/graduation/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.graduation
 
+import RegisterActivity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var registerbtn: Button
+    private lateinit var loginbtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        registerbtn = findViewById(R.id.button1)
+        loginbtn= findViewById(R.id.button2)
+
+        registerbtn.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginbtn.setOnClickListener{
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/graduation/RegisterActivity.kt
@@ -0,0 +1,26 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.graduation.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choosetype)
+
+       // val therapistButton: Button = findViewById(R.id.therapist)
+       // val userButton: Button = findViewById(R.id.user)
+
+        //therapistButton.setOnClickListener {
+
+           // val intent = Intent(this, TherapistActivity::class.java)
+            //startActivity(intent)
+       // }
+
+        //userButton.setOnClickListener {
+
+           // val intent = Intent(this, UserActivity::class.java)
+            //startActivity(intent)
+        //}
+    }
+}
